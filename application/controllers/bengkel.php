c520f78f186af39f06013483d1007ac454f7849d
Luki R Daniel <thekucays@gmail.com>
2015-12-13 16:53:20 +0700

    [update] penambahan fungsi untuk edit bengkel dan text autocomplete (jenis dan kecamatan)

--- application/controllers/bengkel.php
+++ application/controllers/bengkel.php
@@ -26,4 +26,47 @@ class Bengkel extends CI_Controller {
     		redirect('bengkel', 'refresh');
     	}
     }
+
+    function editBengkel($id){
+        $save = $this->input->post('submit_save');
+
+        if ($save){
+            //$this->load->library('form_validation');
+            //$this->form_validation->set_rules('deskripsi','Deskripsi Kendaraan','required');
+
+            //if($this->form_validation->run()){ 
+                $bresult = $this->model_jenis->rubahJenis();
+                if($bresult){
+                    redirect('bengkel/index');
+                }
+            //}
+        }
+
+        $params = array('id_bengkel' => $id);
+        $this->_rubahJenis_form($params);
+    }
+    function _editBengkel_form($params = array()){
+        if( isset($params['id_bengkel']) ){
+            $data['jenis'] = $this->model_bengkel->getDetailBengkel($params);
+            $this->template2->write_view('content', 'bengkel/form_edit_bengkel', $data);
+            $this->template2->render();
+        }
+    }
+
+    // auto complete di form edit nya
+    function jenis_autocomplete(){
+        $term = $this->input->post('term');
+
+        $result = $this->model_so->customer_autocomplete($term);
+        echo json_encode( $result );
+        die();
+    }
+
+    function kecamatan_autocomplete(){
+        $term = $this->input->post('term');
+
+        $result = $this->model_so->customer_autocomplete($term);
+        echo json_encode( $result );
+        die();
+    }
 }
